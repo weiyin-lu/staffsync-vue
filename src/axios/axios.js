6523bfb831cb3bc0317a3d0ca76f14e7a737c7af
weiyin-lu <weiyin2002@outlook.com>
2023-11-08 13:45:21 +0800

    cookie配置

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://127.0.0.1:10000'
+    baseURL: 'http://127.0.0.1:10000',
+    withCredentials: true
 })
 
 http.interceptors.request.use(
