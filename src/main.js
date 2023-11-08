c190dd93dedd03b6e031a513a9e2218b89df5717
weiyin-lu <weiyin2002@outlook.com>
2023-11-08 10:55:13 +0800

    整合vue-router、vuex、element-plus、axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from '/src/router/router'
+import store from "/src/store/store";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router).use(store).use(ElementPlus).mount('#app')
---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+//store.js:
+
+import {createStore} from 'vuex'
+
+const store = createStore({
+    state() {
+        return {}
+    },
+    getters: {},
+    mutations: {},
+    actions: {}
+})
+
+export default store
