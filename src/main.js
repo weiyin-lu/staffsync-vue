66dd445a744867f4da71d70c3cfebbf6e735bcb4
weiyin-lu <weiyin2002@outlook.com>
2023-11-09 17:19:33 +0800

    element-plus本地化配置

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import router from '/src/router/router'
 import store from "/src/store/store";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import api from "./axios/api.js";
 
 const app = createApp(App)
 
-app.provide('$api',api)
+app.provide('$api', api)
 
-app.use(router).use(store).use(ElementPlus).mount('#app')
+app.use(router).use(store).use(ElementPlus, {locale: zhCn}).mount('#app')
