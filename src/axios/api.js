7783c5666e6ee39a88fac218e763f9b0ee33b493
weiyin-lu <weiyin2002@outlook.com>
2023-11-10 15:51:08 +0800

    个人信息页面

--- src/axios/api.js
+++ src/axios/api.js
@@ -4,49 +4,49 @@ import http from "./axios.js";
 const api = {}
 // 认证模块-系统访问认证
 api.login = dto => {
-    return http.post("/authorize/sign/login", dto)
+    return http.post("/authorizes/signs/login", dto)
 }
 api.logout = () => {
-    return http.get("/authorize/sign/logout")
+    return http.get("/authorizes/signs/logout")
 }
 api.register = dto => {
-    return http.post("/authorize/sign/register", dto)
+    return http.post("/authorizes/signs/register", dto)
 }
 // 认证模块-认证信息获取
 api.isLogin = () => {
-    return http.get("/authorize/information/isLogin")
+    return http.get("/authorizes/informations/isLogin")
 }
 // 系统级管理模块-权限系统管理
 api.getPermissionListByPage = value => {
-    return http.get("/admin/permission/getPermissionList/" + value)
+    return http.get("/admins/permissions/getPermissionList/" + value)
 }
 api.addPermission = dto => {
-    return http.post("/admin/permission/addPermission", dto)
+    return http.post("/admins/permissions/addPermission", dto)
 }
 // 系统级管理模块-角色系统管理
 api.getRoleListByPage = value => {
-    return http.get("/admin/role/getRoleList/" + value)
+    return http.get("/admins/roles/getRoleList/" + value)
 }
 api.addRole = dto => {
-    return http.post("/admin/role/addRole", dto)
+    return http.post("/admins/roles/addRole", dto)
 }
 api.setRoleByRolId = dto => {
-    return http.put("/admin/role/updateRole", dto)
+    return http.put("/admins/roles/updateRole", dto)
 }
 api.removeRoleByRoleId = value => {
-    return http.delete("/admin/role/removeRole/" + value)
+    return http.delete("/admins/roles/removeRole/" + value)
 }
 // 系统级管理模块-部门系统管理
 api.getDeptListByPage = value => {
-    return http.get("/admin/department/getDeptList/" + value)
+    return http.get("/admins/departments/getDeptList/" + value)
 }
 api.addDept = dto => {
-    return http.post("/admin/department/addDept", dto)
+    return http.post("/admins/departments/addDept", dto)
 }
 api.setDeptByCode = dto => {
-    return http.put("/admin/department/setDept", dto)
+    return http.put("/admins/departments/setDept", dto)
 }
 api.removeDeptByDeptCode = value => {
-    return http.delete("/admin/department/removeDept/" + value)
+    return http.delete("/admins/departments/removeDept/" + value)
 }
 export default api
--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,9 +1,30 @@
 <script setup>
-
+import {useStore} from "vuex";
+// 全局函数
+const store = useStore()
+// 函数：空值过滤和石化
+const notNullFormat = value => {
+  if (value == null || value == "") {
+    return "无"
+  } else {
+    return value
+  }
+}
 </script>
 
 <template>
-<h1>个人信息</h1>
+  <h1>个人信息</h1>
+  <el-descriptions border direction="vertical" :column="2">
+    <template #extra>
+      <el-button type="primary" plain>维护个人信息</el-button>
+    </template>
+    <el-descriptions-item label="账号">{{ store.state.info.userId }}</el-descriptions-item>
+    <el-descriptions-item label="姓名">{{ store.state.info.name }}</el-descriptions-item>
+    <el-descriptions-item label="联系方式">{{ notNullFormat(store.state.info.phone) }}</el-descriptions-item>
+    <el-descriptions-item label="联系方式">{{ notNullFormat(store.state.info.email) }}</el-descriptions-item>
+    <el-descriptions-item label="部门">{{ notNullFormat(store.state.info.department) }}</el-descriptions-item>
+    <el-descriptions-item label="岗位">{{ notNullFormat(store.state.info.post) }}</el-descriptions-item>
+  </el-descriptions>
 </template>
 
 <style scoped>
