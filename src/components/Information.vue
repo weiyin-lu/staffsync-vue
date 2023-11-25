4bdb3effe29cc0062a5d888456082f81202457e5
weiyin-lu <weiyin2002@outlook.com>
2023-11-25 22:08:09 +0800

    头像上传功能

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -3,6 +3,7 @@ import {useStore} from "vuex";
 import {inject, onMounted, ref} from "vue";
 import http from "../axios/axios.js";
 import {ElMessage} from "element-plus";
+import {UploadFilled} from "@element-plus/icons-vue";
 // 全局函数
 const api = inject("$api")
 const store = useStore()
@@ -16,6 +17,18 @@ const infoData = ref({
 })
 // 标识：维护个人信息对话框
 const infoDialogVisible = ref(false)
+// 标识：头像上传对话框
+const uploadDialogVisible = ref(false)
+// 函数：头像上传成功后 回调操作
+const uploadAfter = (response) => {
+  if (response.code == 200) {
+    store.dispatch('setInfoAction', response.data)
+    ElMessage.success(response.msg);
+    uploadDialogVisible.value = false;
+  } else {
+    ElMessage.error(response.msg + ":" + response.cause)
+  }
+}
 // 函数：个人信息修改 数据装填
 const setInfoBefore = () => {
   infoDialogVisible.value = true
@@ -26,8 +39,8 @@ const setInfoBefore = () => {
 // 函数：修改个人信息
 const setInfo = () => {
   api.setInfoByUserId(infoData.value)
-      .then(r=>{
-        if(r) {
+      .then(r => {
+        if (r) {
           // 把修改后的信息更新到store中
           store.dispatch('setInfoAction', r.data.data)
           ElMessage.success(r.data.data)
@@ -64,7 +77,10 @@ onMounted(() => {
 
 <template>
   <h1>个人信息</h1>
-  <el-avatar :size="100" :src="http.defaults.baseURL + store.state.info.image"/>
+  <el-space direction="vertical">
+    <el-avatar :size="100" :src="http.defaults.baseURL + store.state.info.image"/>
+    <el-button plain @click="uploadDialogVisible = true">修改头像</el-button>
+  </el-space>
   <el-descriptions border direction="vertical" :column="2">
     <template #extra>
       <el-button type="primary" plain @click="setInfoBefore()">维护个人信息</el-button>
@@ -100,6 +116,26 @@ onMounted(() => {
     <el-col style="text-align: center;padding-bottom: 5px">
       <el-button type="success" size="large" plain @click="setInfo()">修改</el-button>
     </el-col>
+  </el-dialog>
+  <!--  头像上传-->
+  <el-dialog v-model="uploadDialogVisible">
+    <template #title>
+      <h1>修改头像</h1>
+    </template>
+    <el-upload drag :action="http.defaults.baseURL + '/information/setHead'" method="PUT"
+               :with-credentials="true" :show-file-list="false" :on-success="uploadAfter">
+      <el-icon class="el-icon--upload">
+        <upload-filled/>
+      </el-icon>
+      <div class="el-upload__text">
+        拖曳上传或<em>点击选择文件</em>
+      </div>
+      <template #tip>
+        <div class="el-upload__tip">
+          jpg/jpeg格式，小于2M
+        </div>
+      </template>
+    </el-upload>
   </el-dialog>
 </template>
 
